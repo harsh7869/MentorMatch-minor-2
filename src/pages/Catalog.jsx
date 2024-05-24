3a47f6715032934c08218ba52fd58e8f74af4a08
Dev Gupta <devgupta210513@acropolis.in>
2024-05-24 23:38:14 +0530

    modifications

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -52,7 +52,7 @@ useEffect(() => {
 
   return (
     <div>
-      <div className=' box-content bg-richblack-800 px-4'>
+      <div className='box-content px-4 bg-richblack-800'>
       <div className='mx-auto flex min-h-[260px]  flex-col justify-center gap-4 '>
         <p className='text-sm text-richblack-300'>Home / Catalog / <span className='text-yellow-25'>{Catalog.catalog}</span> </p>
         <p className='text-3xl text-richblack-5'>{Catalog?.catalog}</p>
@@ -62,29 +62,29 @@ useEffect(() => {
       </div>
       </div>
 
-      <div className=' mx-auto box-content w-full max-w-maxContentTab px-2 py-12 lg:max-w-maxContent'>
+      <div className='box-content w-full px-2 py-12 mx-auto max-w-maxContentTab lg:max-w-maxContent'>
         <h2 className='Courses to get you started'>
         Courses to get you started
         </h2>
-        <div className='my-4 flex border-b border-b-richblack-600 text-sm'>
-          <button onClick={()=>{setActiveOption(1)}}  className={activeOption===1? `px-4 py-2 border-b border-b-yellow-25 text-yellow-25 cursor-pointer`:`px-4 py-2 text-richblack-50 cursor-pointer` }>Most Populer</button>
-          <button onClick={()=>{setActiveOption(2)}} className={activeOption===1?'px-4 py-2 text-richblack-50 cursor-pointer':'px-4 py-2 border-b border-b-yellow-25 text-yellow-25 cursor-pointer'}>New</button>
+        <div className='flex my-4 text-sm border-b border-b-richblack-600'>
+          <button onClick={()=>{setActiveOption(1)}}  className={activeOption===1? `px-4 py-2 border-b border-b-yellow-25 text-yellow-25 cursor-pointer`:`px-4 py-2 text-richblack-500 cursor-pointer` }>Most Populer</button>
+          <button onClick={()=>{setActiveOption(2)}} className={activeOption===1?'px-4 py-2 text-richblack-500 cursor-pointer':'px-4 py-2 border-b border-b-yellow-25 text-yellow-25 cursor-pointer'}>New</button>
         </div>
         <CourseSlider Courses={CatalogPageData?.selectedCourses}/>        
       </div>
 
-      <div className=' mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent'>
-        <h2 className='section_heading mb-6 md:text-3xl text-xl'>
+      <div className='box-content w-full px-4 py-12 mx-auto max-w-maxContentTab lg:max-w-maxContent'>
+        <h2 className='mb-6 text-xl section_heading md:text-3xl text-richblack-500'>
           Similar to {Catalog.catalog}
         </h2>
         <CourseSlider Courses={CatalogPageData?.differentCourses}/>
       </div>
       
-      <div className=' mx-auto box-content w-full max-w-maxContentTab px-2 py-12 lg:max-w-maxContent'>
-        <h2 className='section_heading mb-6 md:text-3xl text-xl'>
+      <div className='box-content w-full px-2 py-12 mx-auto max-w-maxContentTab lg:max-w-maxContent'>
+        <h2 className='mb-6 text-xl section_heading md:text-3xl text-richblack-500'>
           Frequently BoughtTogether
           </h2>
-          <div className='grid grid-cols-2 gap-3 lg:gap-6 lg:grid-cols-2 pr-4'>
+          <div className='grid grid-cols-2 gap-3 pr-4 lg:gap-6 lg:grid-cols-2'>
             {
               CatalogPageData?.mostSellingCourses?.map((item,index)=>(
                 <CatalogCard key={index} course={item} Height={"h-[100px] lg:h-[400px]"} />
